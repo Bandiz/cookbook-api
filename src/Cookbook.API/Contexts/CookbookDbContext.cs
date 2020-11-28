1fc5c2894051e8e613a94451b7c2928e54fb5042
Linas Jakseboga <linas.jakseboga@gmail.com>
2020-11-28 20:11:04 +0100

    added initial Recipe entity; updated startup to initialize EntityFramework

---
+++ src/Cookbook.API/Contexts/CookbookDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Cookbook.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cookbook.API.Contexts
+{
+    public class CookbookDbContext : DbContext
+    {
+        public CookbookDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Recipe> Recipe { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Recipe>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Title).IsRequired();
+            });
+        }
+
+    }
+}
---
+++ src/Cookbook.API/Entities/Recipe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cookbook.API.Entities
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
